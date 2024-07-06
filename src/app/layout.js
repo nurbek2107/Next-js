8f6d158d7a63c44f0cc285fef8c4dfbf5258cf10
Nurbek <161306161+nurbek2107@users.noreply.github.com>
2024-07-06 16:50:40 +0500

    MAIN
    Signed-off-by: Nurbek <161306161+nurbek2107@users.noreply.github.com>

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Navbar from "@/components/Navbar/page";
+import Footer from "@/components/Footer/page";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar/>
+        <main>{children}</main>
+        <Footer/>
+      </body>
     </html>
   );
 }
